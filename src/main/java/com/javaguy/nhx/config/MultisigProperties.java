e5571acdcd498229ed31a2db6246f97a0653f91a
Dancan254 <dancanian25@gmail.com>
2025-11-14 11:13:56 +0300

    feat: Introduce MultisigProperties class for configuration of multisig account settings

---
+++ src/main/java/com/javaguy/nhx/config/MultisigProperties.java
@@ -0,0 +1,15 @@
+package com.javaguy.nhx.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "multisig")
+@Data
+public class MultisigProperties {
+    private String accountId;
+    private List<String> keyList;
+}
