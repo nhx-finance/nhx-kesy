bd4e7f56031b286b62556b9036626cad0763108f
Dancan254 <dancanian25@gmail.com>
2025-11-14 11:14:42 +0300

    feat: Implement UnsignedTransactionService for creating unsigned transactions via multisig API

---
+++ src/main/java/com/javaguy/nhx/service/UnsignedTransactionService.java
@@ -0,0 +1,32 @@
+package com.javaguy.nhx.service;
+
+import com.javaguy.nhx.model.dto.request.UnsignedTransactionRequest;
+import com.javaguy.nhx.model.dto.response.UnsignedTransactionResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UnsignedTransactionService {
+
+    @Value("${multisig.api.url}")
+    private String multisigApiUrl;
+
+    private final RestClient restClient;
+
+    public UnsignedTransactionResponse createUnsignedTransaction(UnsignedTransactionRequest request) {
+        String url = multisigApiUrl + "/v1/transactions";
+        log.info("Sending request to multisig API: {}", url);
+        return restClient.post()
+                .uri(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .retrieve()
+                .body(UnsignedTransactionResponse.class);
+    }
+}
